6a6e006fa4c5eda146221f9f72b9f8b35a445f70
Alba <albapereztomas@gmail.com>
2026-05-18 10:50:43 +0200

    Mejores deck

---
+++ decks_casual.php
@@ -0,0 +1,71 @@
+<?php
+// BASE DE DATOS DE MAZOS CASUALS / FUN / TEMÁTICOS
+$legal_decks = [
+    [
+        'title' => 'Yellow/Green Yamato Aggro',
+        'leader' => 'Yamato (OP06-022)',
+        'tier' => 'C',
+        'colors' => ['yellow', 'green'],
+        'code' => "1xOP06-022\n4xOP06-107\n4xOP06-101\n4xOP04-112\n4xST09-014\n4xOP06-035\n4xOP06-116\n4xOP05-102\n4xOP06-036\n4xOP06-115\n4xOP06-104\n4xOP06-112\n4xOP01-029\n2xOP05-034",
+        'full_list' => [
+            '4x Kouzuki Hiyori (OP06-107)',
+            '4x O-Nami (OP06-101)',
+            '4x Yamato Character (OP04-112)',
+            '4x Charlotte Linlin (ST09-014)',
+            '4x Hiyori (OP06-035)',
+            '4x Reject (OP06-116)',
+            '4x Gedatsu (OP05-102)',
+            '4x Ryuma (OP06-036)',
+            '4x You\'re the One Who Should Disappear! (OP06-115)',
+            '4x Kikunojo (OP06-104)',
+            '4x Green Character (OP06-112)',
+            '4x Radical Beam (OP01-029)',
+            '2x Green Event (OP05-034)'
+        ]
+    ],
+    [
+        'title' => 'Green Foxy Slow Down',
+        'leader' => 'Foxy (OP07-019)',
+        'tier' => 'C',
+        'colors' => ['green'],
+        'code' => "1xOP07-019\n4xOP07-021\n4xOP07-024\n4xOP07-027\n4xOP07-028\n4xOP07-031\n4xOP07-033\n4xOP07-034\n4xOP07-035\n4xOP07-036\n4xOP07-038\n4xOP01-029\n4xST02-007\n2xOP07-039",
+        'full_list' => [
+            '4x Porche (OP07-021)',
+            '4x Hamburg (OP07-024)',
+            '4x Pickles (OP07-027)',
+            '4x Big Pan (OP07-028)',
+            '4x Foxy Character (OP07-031)',
+            '4x It\'s Foxy Sexy Beam! (OP07-033)',
+            '4x Gorilla Punch (OP07-034)',
+            '4x Counter Event (OP07-035)',
+            '4x Green Character (OP07-036)',
+            '4x Green Character (OP07-038)',
+            '4x Radical Beam (OP01-029)',
+            '4x Jewelry Bonney (ST02-007)',
+            '2x Foxy Event (OP07-039)'
+        ]
+    ],
+    [
+        'title' => 'Yellow Vegapunk Egghead',
+        'leader' => 'Dr.Vegapunk (OP07-097)',
+        'tier' => 'C',
+        'colors' => ['yellow'],
+        'code' => "1xOP07-097\n4xOP07-099\n4xOP07-101\n4xOP07-104\n4xOP07-105\n4xOP07-106\n4xOP07-107\n4xOP07-108\n4xOP07-109\n4xOP07-111\n4xOP07-112\n4xOP07-117\n4xOP14-102\n2xOP07-115",
+        'full_list' => [
+            '4x Shaka (OP07-099)',
+            '4x Lilith (OP07-101)',
+            '4x Edison (OP07-104)',
+            '4x Pythagoras (OP07-105)',
+            '4x Atlas (OP07-106)',
+            '4x York (OP07-107)',
+            '4x Stella (OP07-108)',
+            '4x S-Snake (OP07-109)',
+            '4x S-Hawk (OP07-111)',
+            '4x S-Shark (OP07-112)',
+            '4x Punk Records (OP07-117)',
+            '4x Nami (OP14-102)',
+            '2x Yellow Event (OP07-115)'
+        ]
+    ]
+];
+?>
