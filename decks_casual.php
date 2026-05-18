e92177812ce74760bf11f9255e809adefa6e355a
albapereztomas-jpg <albapereztomas@gmail.com>
2026-05-18 11:33:10 +0200

    Add Blue/Black Imu Control deck
    Added a new deck 'Blue/Black Imu Control' with details including leader, tier, colors, tournament placement, and card codes.

--- decks_casual.php
+++ decks_casual.php
@@ -1,71 +1,33 @@
 <?php
 // BASE DE DATOS DE MAZOS CASUALS / FUN / TEMÁTICOS
 $legal_decks = [
+   
     [
-        'title' => 'Yellow/Green Yamato Aggro',
-        'leader' => 'Yamato (OP06-022)',
-        'tier' => 'C',
-        'colors' => ['yellow', 'green'],
-        'code' => "1xOP06-022\n4xOP06-107\n4xOP06-101\n4xOP04-112\n4xST09-014\n4xOP06-035\n4xOP06-116\n4xOP05-102\n4xOP06-036\n4xOP06-115\n4xOP06-104\n4xOP06-112\n4xOP01-029\n2xOP05-034",
-        'full_list' => [
-            '4x Kouzuki Hiyori (OP06-107)',
-            '4x O-Nami (OP06-101)',
-            '4x Yamato Character (OP04-112)',
-            '4x Charlotte Linlin (ST09-014)',
-            '4x Hiyori (OP06-035)',
-            '4x Reject (OP06-116)',
-            '4x Gedatsu (OP05-102)',
-            '4x Ryuma (OP06-036)',
-            '4x You\'re the One Who Should Disappear! (OP06-115)',
-            '4x Kikunojo (OP06-104)',
-            '4x Green Character (OP06-112)',
-            '4x Radical Beam (OP01-029)',
-            '2x Green Event (OP05-034)'
-        ]
-    ],
-    [
-        'title' => 'Green Foxy Slow Down',
-        'leader' => 'Foxy (OP07-019)',
-        'tier' => 'C',
-        'colors' => ['green'],
-        'code' => "1xOP07-019\n4xOP07-021\n4xOP07-024\n4xOP07-027\n4xOP07-028\n4xOP07-031\n4xOP07-033\n4xOP07-034\n4xOP07-035\n4xOP07-036\n4xOP07-038\n4xOP01-029\n4xST02-007\n2xOP07-039",
-        'full_list' => [
-            '4x Porche (OP07-021)',
-            '4x Hamburg (OP07-024)',
-            '4x Pickles (OP07-027)',
-            '4x Big Pan (OP07-028)',
-            '4x Foxy Character (OP07-031)',
-            '4x It\'s Foxy Sexy Beam! (OP07-033)',
-            '4x Gorilla Punch (OP07-034)',
-            '4x Counter Event (OP07-035)',
-            '4x Green Character (OP07-036)',
-            '4x Green Character (OP07-038)',
-            '4x Radical Beam (OP01-029)',
-            '4x Jewelry Bonney (ST02-007)',
-            '2x Foxy Event (OP07-039)'
-        ]
-    ],
-    [
-        'title' => 'Yellow Vegapunk Egghead',
-        'leader' => 'Dr.Vegapunk (OP07-097)',
-        'tier' => 'C',
-        'colors' => ['yellow'],
-        'code' => "1xOP07-097\n4xOP07-099\n4xOP07-101\n4xOP07-104\n4xOP07-105\n4xOP07-106\n4xOP07-107\n4xOP07-108\n4xOP07-109\n4xOP07-111\n4xOP07-112\n4xOP07-117\n4xOP14-102\n2xOP07-115",
-        'full_list' => [
-            '4x Shaka (OP07-099)',
-            '4x Lilith (OP07-101)',
-            '4x Edison (OP07-104)',
-            '4x Pythagoras (OP07-105)',
-            '4x Atlas (OP07-106)',
-            '4x York (OP07-107)',
-            '4x Stella (OP07-108)',
-            '4x S-Snake (OP07-109)',
-            '4x S-Hawk (OP07-111)',
-            '4x S-Shark (OP07-112)',
-            '4x Punk Records (OP07-117)',
-            '4x Nami (OP14-102)',
-            '2x Yellow Event (OP07-115)'
-        ]
-    ]
+            'title' => 'Blue/Black Imu Control',
+            'leader' => 'Imu (OP13-079)',
+            'tier' => 'A',
+            'colors' => ['blue', 'black'],
+            'tournament' => 'EXGrand Tower Of Games',
+            'placement' => '1st Place (5-0)',
+            'pilot' => 'Jacob Haver',
+            'code' => "1xOP13-079\n4xOP13-086\n4xOP13-092\n4xOP13-083\n4xOP13-089\n2xPRB02-014\n4xOP13-080\n4xOP13-091\n4xOP13-084\n4xOP13-082\n4xOP13-096\n2xOP13-097\n4xOP13-098\n4xOP14-096\n1xOP05-097\n1xOP13-099",
+            'full_list' => [
+                '4x Blue/Black Character (OP13-086)',
+                '4x Blue/Black Character (OP13-092)',
+                '4x Blue/Black Character (OP13-083)',
+                '4x Blue/Black Character (OP13-089)',
+                '2x Special Card (PRB02-014)',
+                '4x Blue/Black Character (OP13-080)',
+                '4x Blue/Black Character (OP13-091)',
+                '4x Blue/Black Character (OP13-084)',
+                '4x Blue/Black Character (OP13-082)',
+                '4x Blue/Black Character (OP13-096)',
+                '2x Blue/Black Character (OP13-097)',
+                '4x Blue/Black Character (OP13-098)',
+                '4x Black Character (OP14-096)',
+                '1x Special Card (OP05-097)',
+                '1x Blue/Black Character (OP13-099)'
+            ]
+        ],
 ];
 ?>
