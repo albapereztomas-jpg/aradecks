99a764a55e27a85f65b6138df271744277847100
albapereztomas-jpg <albapereztomas@gmail.com>
2026-05-18 11:44:44 +0200

    Modify deck details for Mono Black Imu Control
    Updated the deck details for the Mono Black Imu Control deck, changing the title, tier, and colors. Adjusted the full list of cards to reflect the new deck composition.

--- decks_casual.php
+++ decks_casual.php
@@ -3,30 +3,30 @@
 $legal_decks = [
    
     [
-            'title' => 'Blue/Black Imu Control',
+            'title' => 'Mono Black Imu Control',
             'leader' => 'Imu (OP13-079)',
-            'tier' => 'A',
-            'colors' => ['blue', 'black'],
+            'tier' => 'C',
+            'colors' => ['black'], // <-- Corregido: Solo color negro
             'tournament' => 'EXGrand Tower Of Games',
             'placement' => '1st Place (5-0)',
             'pilot' => 'Jacob Haver',
             'code' => "1xOP13-079\n4xOP13-086\n4xOP13-092\n4xOP13-083\n4xOP13-089\n2xPRB02-014\n4xOP13-080\n4xOP13-091\n4xOP13-084\n4xOP13-082\n4xOP13-096\n2xOP13-097\n4xOP13-098\n4xOP14-096\n1xOP05-097\n1xOP13-099",
             'full_list' => [
-                '4x Blue/Black Character (OP13-086)',
-                '4x Blue/Black Character (OP13-092)',
-                '4x Blue/Black Character (OP13-083)',
-                '4x Blue/Black Character (OP13-089)',
-                '2x Special Card (PRB02-014)',
-                '4x Blue/Black Character (OP13-080)',
-                '4x Blue/Black Character (OP13-091)',
-                '4x Blue/Black Character (OP13-084)',
-                '4x Blue/Black Character (OP13-082)',
-                '4x Blue/Black Character (OP13-096)',
-                '2x Blue/Black Character (OP13-097)',
-                '4x Blue/Black Character (OP13-098)',
-                '4x Black Character (OP14-096)',
-                '1x Special Card (OP05-097)',
-                '1x Blue/Black Character (OP13-099)'
+                '4x Shepherd Ju Peter (OP13-086)',
+                '4x Marcus Mars (OP13-092)',
+                '4x Ethanbaron V. Nusjuro (OP13-083)',
+                '4x Topman Warcury (OP13-089)',
+                '2x Kuzan (PRB02-014)',
+                '4x Imu (OP13-080)',
+                '4x Jaygarcia Saturn (OP13-091)',
+                '4x Sabo (OP13-084)',
+                '4x Great Eruption (OP13-082)',
+                '4x Six King Pistol (OP13-096)',
+                '2x Tempest Kick: Sky Slicer (OP13-097)',
+                '4x Truth Is Always Cruel (OP13-098)',
+                '4x Enel (OP14-096)',
+                '1x Borsalino (OP05-097)',
+                '1x Imu (OP13-099)'
             ]
         ],
 ];
